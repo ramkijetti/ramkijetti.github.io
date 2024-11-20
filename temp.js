be62f45939d0c70170b760bc22059eee3dfbe669
ramkijetti <ramkijetti>
2024-11-20 17:49:10 +0530

    Updated

---
+++ temp.js
@@ -0,0 +1,19 @@
+db.products.insertMany(
+    [
+    {
+        "name":"Prodcut 5",
+        "desc":"This is the description of product 5",
+        "price":150,
+        "ur1":"https://picsum.photos/id/5/200/200"
+
+    },
+    {
+        "name":"Prodcut 5",
+        "desc":"This is the description of product 6",
+        "price":160,
+        "ur1":"https://picsum.photos/id/6/200/200"
+
+    },
+
+]
+);
